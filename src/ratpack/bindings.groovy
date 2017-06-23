d52e1435f08e7c2c2e62363f25f808a72a680165
Mario Garcia <mario.ggar@gmail.com>
2017-06-23 18:06:48 +0200

    fix add cassandra cluster missing module

--- src/ratpack/bindings.groovy
+++ src/ratpack/bindings.groovy
@@ -1,9 +1,11 @@
+import seguratta.data.DataModule
 import seguratta.auth.AuthModule
 
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
     bindings {
+        module DataModule
         module AuthModule
     }
 }
