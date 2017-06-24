1cfdca0b86fab1688582d27201927d2da293a561
Mario Garcia <mario.ggar@gmail.com>
2017-06-24 08:47:23 +0200

    add groovydoc in migration service

--- src/main/groovy/securatta/data/DataMigrationService.groovy
+++ src/main/groovy/securatta/data/DataMigrationService.groovy
@@ -15,12 +15,27 @@ import org.cognitor.cassandra.migration.MigrationRepository
  */
 class DataMigrationService implements Service {
 
+    /**
+     * Securatta keyspace
+     *
+     * @since 0.1.2
+     */
     static final String SECURATTA_KEYSPACE = 'securatta'
+
+    /**
+     * Query to create Securatta keyspace in Cassandra
+     * @since 0.1.2
+     */
     static final String SECURATTA_CREATE_KEYSPACE = """
        CREATE KEYSPACE IF NOT EXISTS $SECURATTA_KEYSPACE
           WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
     """
 
+    /**
+     * Cassandra cluster connection
+     *
+     * @since 0.1.2
+     */
     @Inject
     Cluster cluster
 
